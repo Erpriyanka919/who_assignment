439da0855c09dacc5d085bd7a531cd712ab40402
Erpriyanka919 <51296689+Erpriyanka919@users.noreply.github.com>
2020-11-29 18:08:43 +0530

    Completed the formatting and verified the successful execution and verification of Assignment_2

--- src/Assignment_2.java
+++ src/Assignment_2.java
@@ -31,7 +31,6 @@ public class Assignment_2 {
 	try {
 	FileUtils.copyFile(src, new File(".\\ImagesForTesting\\Menu.PNG"));
 	} catch (IOException e) {
-  //TODO Auto-generated catch block
 	e.printStackTrace();
 	}
 
@@ -47,26 +46,25 @@ public class Assignment_2 {
 	{
 	FileUtils.copyFile(src1, new File(".\\ImagesForTesting\\Random_Image.PNG"));
 	}catch (IOException e1) {
-  //TODO Auto-generated catch block
 	e1.printStackTrace();
 	}
 		
   //TC-01 - To verify the UI alignment with Mode=Random
 	Finder f1=new Finder(screen.capture().getImage());
 	f1.find(menu);
-	if(f1.hasNext())
-	{
-	Match m=f1.next();
-		if((m.getScore()*100) == 100)
-			System.out.println("No alignment issues found with Random Screen");
-		f1.destroy();
-	}
-	else{
-	 System.out.println("Alignment issues found with Random Screen");
-	 }
+	if (f1.hasNext()) {
+        Match m = f1.next();
+        if ((m.getScore() * 100) == 100){
+            System.out.println("No alignment issues found on Random Screen");
+        } else {
+            System.out.println("Alignment issues found on Random Screen");
+        }
+        f1.destroy();
+    }
+
 		
   //To navigate to UI alignment with pixel_shift=100
-    System.out.println("%nTC-02 - To verify the UI alignment with pixel_shift=100");
+    System.out.println("TC-02 - To verify the UI alignment with pixel_shift=100");
  	driver.get("https://the-internet.herokuapp.com/shifting_content/menu?pixel_shift=100");
 
   //Save the pixel_shift=100 Screen Screenshot	
@@ -74,27 +72,27 @@ public class Assignment_2 {
 	try 
 	{
 	FileUtils.copyFile(src2, new File(".\\ImagesForTesting\\PixelShift_Image.PNG"));
-	}catch (IOException e1) {
-  //TODO Auto-generated catch block
-	e1.printStackTrace();
+	}catch (IOException e2) {
+	e2.printStackTrace();
 	 }
 		
 
   //TC-02 - To verify the UI alignment with pixel_shift=100
     Finder f2=new Finder(screen.capture().getImage());
 	f2.find(menu);
-    if(f2.hasNext()){
-    Match m=f2.next();
-    if((m.getScore()*100) == 100)
-    System.out.println("No alignment issues found with pixel_shift=100 Screen");
-    f2.destroy();
-    }
-    else{
-    System.out.println("Alignment issues found with pixel_shift=100 Screen");
+	if (f2.hasNext()) {
+        Match m = f2.next();
+        if ((m.getScore() * 100) == 100){
+            System.out.println("No Alignment issues found with pixel_shift=100 Screen");
+        } else {
+            System.out.println("Alignment issues found with pixel_shift=100 Screen");
+        }
+        f2.destroy();
     }
 
+
   //To navigate to UI alignment with mode=random&pixel_shift=100
-    System.out.println("%nTC-03 - To verify the UI alignment with mode=random&pixel_shift=100");
+    System.out.println("TC-03 - To verify the UI alignment with mode=random&pixel_shift=100");
  	driver.get("https://the-internet.herokuapp.com/shifting_content/menu?mode=random&pixel_shift=100");
 
   //Save the mode=random&pixel_shift=100 Screen Screenshot	
@@ -103,25 +101,24 @@ public class Assignment_2 {
 	{
 	FileUtils.copyFile(src3, new File(".\\ImagesForTesting\\RandomPixelShift_Image.PNG"));
 	}catch (IOException e3) {
-  //TODO Auto-generated catch block
 	e3.printStackTrace();
 	}
 
   //TC-03 - To verify the UI alignment with mode=random&pixel_shift=100
   	Finder f3=new Finder(screen.capture().getImage());
 	f3.find(menu);
-	if(f3.hasNext()){
-	Match m=f3.next();
-	if((m.getScore()*100) == 100)
-	System.out.println("No alignment issues found with mode=random&pixel_shift=100 Screen");
-	f3.destroy();
-	}
-	else{
-	System.out.println("Alignment issues found  with mode=random&pixel_shift=100 Screen");
-	}
-	    
+	if (f3.hasNext()) {
+        Match m = f3.next();
+        if ((m.getScore() * 100) == 100){
+            System.out.println("No alignment issues found with mode=random&pixel_shift=100 Screen");
+        } else {
+            System.out.println("Alignment issues found  with mode=random&pixel_shift=100 Screen");
+        }
+        f3.destroy();
+    }
+
   //Refresh the screen
-	System.out.println("%nTC-04 - To verify the UI alignment with the base screen on selecting Refresh(F5)");
+	System.out.println("TC-04 - To verify the UI alignment with the base screen on selecting Refresh(F5)");
 	driver.get("https://the-internet.herokuapp.com/shifting_content/menu");
 	driver.navigate().refresh();
   //Save the Screenshot	on selecting Refresh(F5)
@@ -130,24 +127,24 @@ public class Assignment_2 {
 	{
 	FileUtils.copyFile(src4, new File(".\\ImagesForTesting\\Refresh_Image.PNG"));
 	}catch (IOException e4) {
-  // TODO Auto-generated catch block
 	e4.printStackTrace();
 	}
 			
   //TC-04 - To verify the UI alignment with the base screen on selecting Refresh(F5)
     Finder f4=new Finder(screen.capture().getImage());
 	f4.find(menu);
-	if(f4.hasNext()){
-	Match m=f4.next();
-	if((m.getScore()*100) == 100)
-	System.out.println("No alignment issues found on Refresh Screen");
-	f4.destroy();
+	if (f4.hasNext()) {
+        Match m = f4.next();
+        if ((m.getScore() * 100) == 100){
+            System.out.println("No alignment issues found on Refresh Screen");
+        } else {
+            System.out.println("Alignment issues found on Refresh Screen");
+        }
+        f4.destroy();
+    }
+    driver.quit();
 	}
-	else{
-	System.out.println("Alignment issues found found on Refresh Screen");
-
 	}
-	}
-	}
+	
 		
 
